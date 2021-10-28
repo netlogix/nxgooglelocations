d221a36ec2380feedb7561f7310bf059902579ae
Stephan Schuler <stephan.schuler@netlogix.de>
2021-10-28 08:11:05 +0200

    TASK: Allow several instances of TableListViewHelper
    This drops the RecordTableViewHelper entirely. The Backend\TableList
    ViewHelper provides this feature.
    This provides an additional TableList ViewHelper which allows for
    several record lists on the same page as long as they don't show
    the same table.
    
    Related: UVEXGROUP-2268

--- Classes/ViewHelpers/Be/TableListViewHelper.php
+++ Classes/ViewHelpers/Be/TableListViewHelper.php
@@ -2,11 +2,30 @@
 
 namespace Netlogix\Nxgooglelocations\ViewHelpers\Be;
 
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 class TableListViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Be\TableListViewHelper
 {
-    public function render()
+    public function render(): string
     {
-        $result = parent::render();
-        return is_int(strpos($result, '<table')) ? $result : $this->renderChildren();
+        $tableName = $this->arguments['tableName'];
+        $preventPointer = $tableName !== GeneralUtility::_GP('table');
+
+        try {
+            if ($preventPointer) {
+                $backup = [
+                    'get' => $_GET,
+                    'post' => $_POST
+                ];
+                $_GET['pointer'] = 0;
+                $_POST['pointer'] = 0;
+            }
+            $result = parent::render();
+            return is_int(strpos($result, '<table')) ? $result : $this->renderChildren();
+
+        } finally {
+            $_GET = $backup['get'];
+            $_POST = $backup['post'];
+        }
     }
 }
