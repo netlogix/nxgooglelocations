06115d36c9c9e8a622f254dcfea8d797f66a63d1
Stephan Schuler <stephan.schuler@netlogix.de>
2021-10-28 07:59:36 +0200

    TASK: Raise dependencies and version number
    This extension now depends on PHP 7.4 and TYPO3 10.4.
    
    Related: UVEXGROUP-2268

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,9 +22,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '8.7.0',
     'constraints' => [
         'depends' => [
-            'cms' => '',
-            'php' => '5.5.0-0.0.0',
-            'typo3' => '8.7.0-8.99.99',
+            'php' => '7.4.0-7.4.99',
+            'typo3' => '10.4.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
