6bc2848c0612b69094c6139836bb3c9562fa7905
Tim Weisenberger <tim.weisenberger@netlogix.de>
2023-12-06 11:06:36 +0100

    refactor: Adjust extension basis to current best practices

---
+++ Tests/Functional/DummyTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netlogix\Nxgooglelocations\Tests\Functional;
+
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class DummyTest extends FunctionalTestCase
+{
+    #[Test]
+    public function dummyTest(): void
+    {
+        self::assertTrue(true);
+    }
+}
