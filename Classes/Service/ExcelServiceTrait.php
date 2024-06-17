3add36af57cf27e0c152c43e8f32bca3e38fddd4
Sascha Nowak <sascha.nowak@netlogix.de>
2024-06-17 17:03:16 +0200

    fix: LocationFactory and ExcelServiceTrait define the same property ($templateSheet) in the composition of LocationFactory

--- Classes/Service/ExcelServiceTrait.php
+++ Classes/Service/ExcelServiceTrait.php
@@ -11,9 +11,9 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 trait ExcelServiceTrait
 {
-    protected ?Worksheet $templateSheet;
+    protected ?Worksheet $templateSheet = null;
 
-    protected ?Worksheet $contentSheet;
+    protected ?Worksheet $contentSheet = null;
 
     public function resetTemplateSheet(string $templateFileName): void
     {
